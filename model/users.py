7508c1ef11ce771faf301984bc1ae19d78b643f0
tejnc <tejnarayan15.tnc@gmail.com>
2021-11-11 10:06:13 +0545

    user model and db conn

--- model/users.py
+++ model/users.py
@@ -5,10 +5,10 @@ from mongoengine.fields import DateTimeField, DictField, EmailField, StringField
 
 class Users(Document):
     name = StringField(required=True)
-    gender = StringField(choices=("male","female","other"))
+    gender = StringField(choices=("male", "female", "other"))
     phone_number = StringField()
     address = DictField()
-    username = StringField(required=True, unique=True,min_length=3,max_length=16)
-    email = EmailField(required=True,unique=True)
-    password = StringField(required=True,min_length=8)
+    username = StringField(required=True, unique=True, min_length=3, max_length=16)
+    email = EmailField(required=True, unique=True)
+    password = StringField(required=True, min_length=8)
     created_at = DateTimeField(default=datetime.datetime.utcnow)
--- utils/mongo.py
+++ utils/mongo.py
@@ -1,10 +1,12 @@
-import os
 from mongoengine import connect
+
+
+from utils.constants import MONGO_PASS, MONGO_DB
+
 
 def get_db_connection():
     url = "mongodb+srv://test:{}@cluster0.x25kn.mongodb.net/{}?retryWrites=true&w=majority".format(
-        os.environ["MONGO_PASS"],
-        os.environ["MONGO_DB"]
+        MONGO_PASS, MONGO_DB
     )
     return url
 
@@ -14,4 +16,4 @@ def db_config():
         uri: str = get_db_connection()
         connect(host=uri)
     except KeyError:
-        connect("mongoenginetest",host="mongomock://localhost")
+        connect("mongoenginetest", host="mongomock://localhost")
